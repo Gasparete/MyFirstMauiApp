4787ceee6af4707c6cd8ed8c48f87b940575d83e
vitor <vitor@DESKTOP-1TFR6TD>
2023-02-19 22:31:01 -0300

    Add project files.

---
+++ MyFirstMauiApp/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MyFirstMauiApp.ViewModel;
+
+namespace MyFirstMauiApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage(MainViewModel vm)
+	{
+		InitializeComponent();
+		BindingContext = vm; // With Injection Dependency
+
+        // Similar way to do
+        // BindingContext = new MainViewModel();
+    }
+}
+
---
+++ MyFirstMauiApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstMauiApp.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        ObservableCollection<string> items;
+
+        [ObservableProperty]
+        string text;
+
+        public MainViewModel()
+        {
+            items = new();
+        }
+
+        [RelayCommand]
+        void Add()
+        {
+            if (string.IsNullOrWhiteSpace(Text))
+                return;
+
+            Items.Add(Text);
+            Text = string.Empty;
+        }
+
+        [RelayCommand]
+        void Delete(string s)
+        {
+            if (Items.Contains(s))
+                Items.Remove(s);
+        }
+
+        [RelayCommand]
+        async Task Tap(string s)
+        {
+            await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={s}");
+        }
+    }
+}
